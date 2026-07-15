7f301cf3955804d9eb85ee2a1c29133b9c565f1d
Niraj Vilkar <nirajvilkar272@gmail.com>
2026-07-16 00:06:06 +0530

    Constants in Scipy

---
+++ constants.py
@@ -0,0 +1,11 @@
+import scipy
+from scipy.constants import constants
+
+# All the constants available in Scipy
+print(dir(constants)) 
+
+# Common examples 
+print(constants.pi)
+print(constants.speed_of_light)
+print(constants.eV)
+
